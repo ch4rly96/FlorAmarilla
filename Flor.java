c157bad41e286e8d7767fc32febf4683d6be6b9d
ch4rly96 <charlyvg240896@gmail.com>
2026-04-24 18:18:24 -0500

    Añadiendo el codigo

--- Flor.java
+++ Flor.java
@@ -2,18 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Flor extends JPanel {
-    @Override
+@Override
+
     protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     g.setColor(Color.YELLOW);
-    g.fillOval(100, 50, 100, 100); // pétalo superior
-    g.fillOval(50, 100, 100, 100); // pétalo izquierdo
-    g.fillOval(150, 100, 100, 100); // pétalo derecho
-    g.fillOval(100, 150, 100, 100); // pétalo inferior
+    g.fillOval(100, 50, 100, 100); // petalo superior
+    g.fillOval(50, 100, 100, 100); // petalo izquierdo
+    g.fillOval(150, 100, 100, 100); // petalo derecho
+    g.fillOval(100, 150, 100, 100); // petalo inferior
     g.setColor(Color.ORANGE);
     g.fillOval(110, 110, 80, 80); // centro
     }
-
     public static void main(String[] args) {
     JFrame frame = new JFrame("Flor Amarilla 🌼");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,4 +21,5 @@ public class Flor extends JPanel {
     frame.add(new Flor());
     frame.setVisible(true);
     }
+    
 }
