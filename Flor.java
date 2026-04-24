1fdf2bacc8169b69ee8e68cbae936bda23b6deb1
ch4rly96 <charlyvg240896@gmail.com>
2026-04-24 18:47:29 -0500

    Problema resuelto

--- Flor.java
+++ Flor.java
@@ -10,7 +10,8 @@ public class Flor extends JPanel {
     g.fillOval(100, 50, 100, 100); // petalo superior
     g.fillOval(50, 100, 100, 100); // petalo izquierdo
     g.fillOval(150, 100, 100, 100); // petalo derecho
-    g.fillOval(100, 150, 100, 100); // petalo inferio
+    g.fillOval(100, 150, 100, 100); // petalo inferior
+    g.setColor(Color.red);
     g.fillOval(110, 110, 80, 80); // centro
     }
     public static void main(String[] args) {
